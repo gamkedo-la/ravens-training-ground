bc4fefc103374117b232355bf94caeb1f308e5b6
DaniAntSinc <cub.noble@yahoo.fr>
2023-06-16 21:14:34 -0500

    Creating 'Actions'. Adding them to characters.

--- Assets/Scripts/Attacks/AttackBase.cs
+++ Assets/Scripts/Attacks/AttackBase.cs
@@ -6,7 +6,7 @@ using UnityEngine;
 public enum AttackCosts { tier0 = 0, tier1 = 4, tier2 = 8, tier3 = 12, tier4 = 15, tier5 = 18, tier6 = 21 }
 public enum CastType { Friendly, Enemy }
 public enum TargetType { SingleTarget, AOE }
-public enum EffectType { Charms, Physical, DarkArts, Transfiguration, Ancient }
+public enum EffectType { Charms, Physical, DarkArts, Transfiguration, Ancient, None }
 public enum AttackExcersion { LightAttack, MediumAttack, DefaultAttack }
 [CreateAssetMenu(fileName ="Attack",menuName ="Actions/Attack")]
 public class AttackBase : ScriptableObject
@@ -48,6 +48,8 @@ public class AttackBase : ScriptableObject
                 return unit.Magic;
             case EffectType.Ancient:
                 return unit.Magic;
+            case EffectType.None:
+                return unit.Magic;
             default:
                 Debug.Log("Missing Type");
                 return 0;
@@ -67,6 +69,8 @@ public class AttackBase : ScriptableObject
             case EffectType.Transfiguration:
                 return 0;
             case EffectType.Ancient:
+                return 0;
+            case EffectType.None:
                 return 0;
             default:
                 Debug.Log("Missing Type");
