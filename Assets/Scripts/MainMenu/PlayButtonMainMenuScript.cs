3af9d1a753c49f1aaee8a2612c34e42e617970fb
Steven Solof <stebssbets@yahoo.com>
2023-07-02 12:47:06 -0400

    Added UI hover to main menu buttons

---
+++ Assets/Scripts/MainMenu/PlayButtonMainMenuScript.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayButtonMainMenuScript : MonoBehaviour
+{
+    public void HandleClick()
+    {
+        Debug.Log("how does the game start?");
+    }
+}
