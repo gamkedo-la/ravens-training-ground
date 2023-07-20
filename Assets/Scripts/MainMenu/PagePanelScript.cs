b56f826cd6212f3f5a3a61cc424be8c342306ad9
Steven Solof <stebssbets@yahoo.com>
2023-07-20 18:46:49 -0400

    basic screenshot functionality of help menu

--- Assets/Scripts/MainMenu/PagePanelScript.cs
+++ Assets/Scripts/MainMenu/PagePanelScript.cs
@@ -18,20 +18,43 @@ public class PagePanelScript : MonoBehaviour
     [SerializeField] List<Sprite> listOfHelpPageScreenshots = new List<Sprite>();
 
     [SerializeField] Image screenshotImageGameObject;
+    private Image actualImageComponent;
     [SerializeField] TextMeshProUGUI helpTextGameObject;
 
     [SerializeField] Button backButton;
     [SerializeField] Button forwardButton;
 
+    private int numberOfScreenshots;
+    private int currentListOfScreenShotsIndex = 0;
+
     #endregion
-    
+
+    private void Start()
+    {
+        numberOfScreenshots = listOfHelpPageScreenshots.Count;
+    }
+
     public void HandleBackButtonClick()
     {
+        currentListOfScreenShotsIndex -= 1;
+        if (currentListOfScreenShotsIndex < 0)
+        {
+            currentListOfScreenShotsIndex = 0;
+        }
 
+        screenshotImageGameObject.sprite = listOfHelpPageScreenshots[currentListOfScreenShotsIndex];
+        helpTextGameObject.text = listOfHelpPageStrings[currentListOfScreenShotsIndex];
     }
 
     public void HandleForwardButtonClick()
     {
+        currentListOfScreenShotsIndex += 1;
+        if (currentListOfScreenShotsIndex > numberOfScreenshots - 1)
+        {
+            currentListOfScreenShotsIndex = numberOfScreenshots - 1;
+        }
 
+        screenshotImageGameObject.sprite = listOfHelpPageScreenshots[currentListOfScreenShotsIndex];
+        helpTextGameObject.text = listOfHelpPageStrings[currentListOfScreenShotsIndex];
     }
 }
