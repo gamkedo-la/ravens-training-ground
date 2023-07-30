c5de86c20d870fb54ffc6d05791c3397dd90be69
DaniAntSinc <cub.noble@yahoo.fr>
2023-07-30 10:24:07 -0500

    Tweaking Floor0, adding animation to overworld

--- Assets/Scripts/OverworldMovement.cs
+++ Assets/Scripts/OverworldMovement.cs
@@ -4,16 +4,31 @@ using UnityEngine;
 
 public class OverworldMovement : MonoBehaviour
 {
-    float speed = .01f;
+    float speed = .025f;
     bool cantMove;
     public Animator anim;
+    public float turnSmoothTime = 0.1f;
+    float turnSmoothVelocity;
 
     void Update()
     {
-        float horizontal = Input.GetAxisRaw("Horizontal");
-        float vertical = Input.GetAxisRaw("Vertical");
+        float horizontal = -Input.GetAxisRaw("Horizontal");
+        float vertical = -Input.GetAxisRaw("Vertical");
         Vector3 direction = new Vector3(horizontal, 0f, vertical);
 
-        gameObject.transform.position = new Vector3(transform.position.x + (horizontal * -speed), -2.4f, transform.position.z + (vertical * -speed));
+        gameObject.transform.position = new Vector3(transform.position.x + (horizontal * speed), -2.4f, transform.position.z + (vertical * speed));
+
+        if (direction.magnitude >= 0.1)
+        {
+            float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg;
+            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
+            transform.rotation = Quaternion.Euler(0f, angle, 0f);
+
+            Vector3 moveDirection = Quaternion.Euler(0, targetAngle, 0) * Vector3.forward;
+
+            anim.SetBool("isRunning", true);
+        }
+        else
+            anim.SetBool("isRunning", false);
     }
 }
