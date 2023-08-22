65463bfca731b22c304113586e353243d61bf91d
CTGabianelli11 <ctgabbianelli@gmail.com>
2023-08-22 17:19:10 -0400

    Added killer to custom health editor

--- Assets/Editor/HealthCustomEditor.cs
+++ Assets/Editor/HealthCustomEditor.cs
@@ -5,13 +5,24 @@ using UnityEngine;
 [CustomEditor(typeof(Health))]
 public class HealthCustomEditor : Editor
 {
+    [SerializeField] Unit killer;
     public override void OnInspectorGUI()
     {
         base.OnInspectorGUI();
         Health healthScript = (Health)target;
-        if(GUILayout.Button("Kill"))
+
+        EditorGUILayout.LabelField("Death Debugging", EditorStyles.boldLabel);
+        
+        killer =EditorGUILayout.ObjectField("Killer", killer, typeof(Unit), true) as Unit;
+        if (GUILayout.Button("Kill"))
         {
-            healthScript.TakeDamage(null,healthScript.GetCurrentHP());
+            if(killer == null)
+            {
+                Debug.LogError("Missing Killer");
+                return;
+            }
+
+            healthScript.TakeDamage(killer,healthScript.GetCurrentHP());
         }
     }
 }
