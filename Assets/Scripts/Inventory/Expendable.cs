25be26c0450669928bcb790e7de0c0d5b4e09d49
DaniAntSinc <cub.noble@yahoo.fr>
2023-09-04 10:10:06 -0500

    Creating expendable inventory item scriptable objects

---
+++ Assets/Scripts/Inventory/Expendable.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using Character.Stats;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Expendable", menuName = "Actions/Expendable")]
+
+public class Expendable : InventoryItemBase
+{
+    //affect one for the party
+    public bool solo, party;
+    //increases or decreases stats
+    public bool boost, detriment;
+    //reincarnate
+    public bool reincarnate;
+    //specific stat affected
+    public bool magicPoints, health, attack, defense;
+    //affecting a direct number (like 50 health) or a percent (20% attack increase)
+    public int specificAmount, percentageAmount; 
+}
