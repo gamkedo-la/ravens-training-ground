7ff70c8705d4f7f7261690cd3657b3c808da2a46
DaniAntSinc <cub.noble@yahoo.fr>
2023-09-09 16:55:57 -0500

    Laying out UI foundation for Clothing Part 2

--- Assets/PullPlayersClothing.cs
+++ Assets/PullPlayersClothing.cs
@@ -1,36 +1,73 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class PullPlayersClothing : MonoBehaviour
 {
     public GameObject[] wands, maleShirt, malePants, maleHat, femaleShirt, femalePants, femaleHat;
     public Clothing specificClothing;
 
+    TMP_Text itemName, stock;
+
+    public GameObject wandPage, hatPage, shirtPage, pantsPage;
+
+    private void Start()
+    {
+        itemName = gameObject.transform.GetChild(2).GetComponent<TMP_Text>();
+        stock = gameObject.transform.GetChild(1).GetComponent<TMP_Text>();
+
+        itemName.text = specificClothing.nameOfItem;
+        stock.text = specificClothing.playerStock.ToString("F0");
+    }
+
+    public void ToggleWands()
+    {
+        GameManager.ClearGameManagerDataFromInventory();
+        TurnAllOff();
+
+        wandPage.SetActive(true);
+        hatPage.SetActive(false);
+        shirtPage.SetActive(false);
+        pantsPage.SetActive(false);
+    }
+
+    public void ToggleHats()
+    {
+        GameManager.ClearGameManagerDataFromInventory();
+        TurnAllOff();
+
+        wandPage.SetActive(false);
+        hatPage.SetActive(true);
+        shirtPage.SetActive(false);
+        pantsPage.SetActive(false);
+    }
+
+    public void ToggleShirts()
+    {
+        GameManager.ClearGameManagerDataFromInventory();
+        TurnAllOff();
+
+        wandPage.SetActive(false);
+        hatPage.SetActive(false);
+        shirtPage.SetActive(true);
+        pantsPage.SetActive(false);
+    }
+
+    public void TogglePants()
+    {
+        GameManager.ClearGameManagerDataFromInventory();
+        TurnAllOff();
+
+        wandPage.SetActive(false);
+        hatPage.SetActive(false);
+        shirtPage.SetActive(false);
+        pantsPage.SetActive(true);
+    }
+
     public void TurnSlotsOn()
     {
-        #region Turn All of them off for leftover data
-        for (int i = 0; i < wands.Length; i++)
-            wands[i].SetActive(false);
-
-        for (int i = 0; i < maleShirt.Length; i++)
-            maleShirt[i].SetActive(false);
-
-        for (int i = 0; i < femaleShirt.Length; i++)
-            femaleShirt[i].SetActive(false);
-
-        for (int i = 0; i < malePants.Length; i++)
-            malePants[i].SetActive(false);
-
-        for (int i = 0; i < femalePants.Length; i++)
-            femalePants[i].SetActive(false);
-
-        for (int i = 0; i < maleHat.Length; i++)
-            maleHat[i].SetActive(false);
-
-        for (int i = 0; i < femaleHat.Length; i++)
-            femaleHat[i].SetActive(false);
-        #endregion
+        TurnAllOff();
         #region Hat
         if (specificClothing.equipableSlot == "head" && specificClothing.gender == "male")        {
             for (int i = 0; i < maleHat.Length; i++)
@@ -93,5 +130,31 @@ public class PullPlayersClothing : MonoBehaviour
         }
         #endregion
         GameManager.clothingName = specificClothing;
+    }
+
+    void TurnAllOff()
+    {
+        #region Turn All of them off for leftover data
+        for (int i = 0; i < wands.Length; i++)
+            wands[i].SetActive(false);
+
+        for (int i = 0; i < maleShirt.Length; i++)
+            maleShirt[i].SetActive(false);
+
+        for (int i = 0; i < femaleShirt.Length; i++)
+            femaleShirt[i].SetActive(false);
+
+        for (int i = 0; i < malePants.Length; i++)
+            malePants[i].SetActive(false);
+
+        for (int i = 0; i < femalePants.Length; i++)
+            femalePants[i].SetActive(false);
+
+        for (int i = 0; i < maleHat.Length; i++)
+            maleHat[i].SetActive(false);
+
+        for (int i = 0; i < femaleHat.Length; i++)
+            femaleHat[i].SetActive(false);
+        #endregion
     }
 }
