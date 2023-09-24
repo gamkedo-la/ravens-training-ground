9e28674bff32d224a2ab3b9ecd793551c68d66e8
DaniAntSinc <cub.noble@yahoo.fr>
2023-09-24 10:28:15 -0500

    Chest functionality/display in dungeon. Trimming clothing/items options.

--- Assets/Scripts/dungeonChest.cs
+++ Assets/Scripts/dungeonChest.cs
@@ -1,21 +1,65 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class dungeonChest : MonoBehaviour
 {
     bool hasEntered;
     Animator anim;
+
+    GameObject gameManager;
+
+    public int shinyMin, shinyCount, shinyMax;
+
+    public TMP_Text shinyText, itemText;
+
+    int randomlySelect;
+    string split;
+    public Expendable[] InventoryPool;
+
+    float countDownTime = 3;
+    bool beginCountdown;
     private void Start()
     {
         anim = GetComponent<Animator>();
+        gameManager = GameObject.Find("GameManager");
     }
     private void Update()
     {
         if (hasEntered && Input.GetKeyDown(KeyCode.Space))
         {
             anim.SetBool("hasOpened", true);
-            print("OpenChest");
+
+            SelectInvItem();
+            if (gameManager.GetComponent<GameManager>().playerInventory.Contains(InventoryPool[randomlySelect]))
+            {
+                InventoryPool[randomlySelect].playerStock += 1;
+            }
+
+            else
+                gameManager.GetComponent<GameManager>().playerInventory.Add(InventoryPool[randomlySelect]);
+
+            shinyText.text = shinyCount.ToString();
+            split = InventoryPool[randomlySelect].ToString();
+
+            split = split.Replace("(Expendable)", "");
+
+            itemText.text = split;
+
+            countDownTime = 3;
+            beginCountdown = true;
+        }
+
+        if (beginCountdown)
+        {
+            countDownTime -= Time.deltaTime;
+            if (countDownTime <= 0)
+            {
+                shinyText.text = "";
+                itemText.text = "";
+            }
+
         }
     }
 
@@ -32,5 +76,13 @@ public class dungeonChest : MonoBehaviour
         {
             hasEntered = false;
         }
+    }
+
+    public void SelectInvItem()
+    {
+        randomlySelect = Random.Range(0, InventoryPool.Length);
+        shinyCount = Random.Range(shinyMin, shinyMax);
+
+        GameManager.shinyThings += shinyCount;
     }
 }
