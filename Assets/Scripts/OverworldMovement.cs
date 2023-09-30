943175e3be38d33d846f444b0669b6a7198f745a
DaniAntSinc <cub.noble@yahoo.fr>
2023-09-30 10:44:09 -0500

    Overhauling Floor0 to better fit the theme

--- Assets/Scripts/OverworldMovement.cs
+++ Assets/Scripts/OverworldMovement.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class OverworldMovement : MonoBehaviour
 {
-    float speed = 2.5f;
+    float speed = 4f;
     bool cantMove;
     public Animator anim;
     public float turnSmoothTime = 0.1f;
